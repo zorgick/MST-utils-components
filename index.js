47526ba1fcfce06aa20491607348392ada6b115f
zorgick <zorgizorgik@gmail.com>
2020-12-01 00:17:07 +0300

    Link dependant fields

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import InputLabel from "@material-ui/core/InputLabel";
 import MenuItem from "@material-ui/core/MenuItem";
 import TextField from "@material-ui/core/TextField";
 import Select from "@material-ui/core/Select";
+import Button from "@material-ui/core/Button";
 
 export const FormValue = types.model("FormValue", {
     id: types.identifier,
@@ -18,14 +19,14 @@ export const FormValue = types.model("FormValue", {
 export const FormField = types
     .model("FormField", {
         id: types.identifier,
-        label: types.string,
+        label: types.optional(types.string, ""),
         disabled: types.optional(types.boolean, false),
         required: types.optional(types.boolean, false),
         error: types.maybe(types.string),
         fieldIndex: types.maybe(types.string),
         values: types.optional(types.array(FormValue), []),
         singleValue: types.optional(types.string, ""),
-        selected: types.maybe(types.reference(types.late(() => FormValue))),
+        selected: types.maybe(types.safeReference(types.late(() => FormValue))),
         // if the field has a dependent field assign it on a initialisation
         next: types.maybeNull(types.reference(types.late(() => FormField)))
     })
@@ -33,18 +34,24 @@ export const FormField = types
         updateFunc: undefined
     }))
     .actions((self) => ({
-        setSelection(selected) {
+        setSelection(selected, isInternalSet) {
             // selection of multiple values is possible
             // only when array of values has items
-            if (self.values.length > 0) {
+            // OR this condition is met when setSelection
+            // is called from the inside of the model
+            if (self.values.length > 0 || isInternalSet) {
+                // refernce must be reseted before
+                // values are updated
+                // OR select supports an empty selection
                 if (!selected) {
                     self.selected = undefined;
                 } else {
                     self.selected = selected;
-
-                    // if (self.next) {
-                    //   self.next.updateValues({ id: selected });
-                    //}
+                }
+                // doesn't matter if a selected variable received a reference or not
+                // require an update for a child
+                if (self.next) {
+                    self.next.updateValues({ id: selected });
                 }
             } else {
                 self.singleValue = selected;
@@ -61,15 +68,15 @@ export const FormField = types
             self.updateFunc = updateFunc;
         },
         updateValues: flow(function* (params) {
-            if (self.selected) {
-                self.selected = undefined;
-            }
             if (!self.disabled) {
                 self.disabled = true;
             }
-            const newValues = yield self.updateFunc(params);
-            self.values = cast(newValues);
-            self.disabled = false;
+            self.setSelection(undefined, true);
+            if (params.id) {
+                const newValues = yield self.updateFunc(params);
+                self.values = cast(newValues);
+                self.disabled = false;
+            }
         })
     }));
 
@@ -105,6 +112,29 @@ const RootStore = types
         }
     }))
     .actions((self) => ({
+        getAnotherData: flow(function* ({ id }) {
+            yield new Promise((resolve) => {
+                setTimeout(resolve, 3000);
+            }).then((result) => {});
+            const values = [
+                {
+                    id: "idite",
+                    value: "idite",
+                    name: "Я вас не звал! Идите нахуй!"
+                },
+                {
+                    id: "hi",
+                    value: "hi",
+                    name: "Дратути!"
+                },
+                {
+                    id: "netochno",
+                    value: "netochno",
+                    name: "Но это не точно"
+                }
+            ];
+            return values;
+        }),
         getData: flow(function* ({ id }) {
             yield new Promise((resolve) => {
                 setTimeout(resolve, 3000);
@@ -131,9 +161,9 @@ const RootStore = types
     }))
     .actions((self) => ({
         afterCreate() {
-            self.formValues.set("statusId", {
-                id: "statusId",
-                label: "Статус",
+            self.formValues.set("Category1", {
+                id: "Category1",
+                label: "Мемы 1й категории",
                 disabled: false,
                 required: true,
                 fieldIndex: "1",
@@ -148,7 +178,52 @@ const RootStore = types
                 ],
                 selected: "pazani"
             });
-            self.formValues.get("statusId").setUpdateFunc(self.getData);
+            self.formValues.set("Category2", {
+                id: "Category2",
+                label: "Мемы 2й категории",
+                disabled: false,
+                required: true,
+                fieldIndex: "2",
+                values: [
+                    { name: "Мутный тип", value: "tip", id: "tip" },
+                    { name: "Пойдем отсюда", value: "go", id: "go" },
+                    {
+                        name: "Долбит нормально!",
+                        value: "dolbit",
+                        id: "dolbit"
+                    }
+                ],
+                selected: "dolbit"
+            });
+            self.formValues.set("Category3", {
+                id: "Category3",
+                label: "Мемы 3й категории",
+                disabled: false,
+                required: true,
+                fieldIndex: "3",
+                values: [
+                    {
+                        name: "Повар спрашивает повара",
+                        value: "povar",
+                        id: "povar"
+                    },
+                    {
+                        name: "Водочки нам принеси",
+                        value: "vodochki",
+                        id: "vodochki"
+                    },
+                    {
+                        name: "В ебыч прописать?",
+                        value: "ebich",
+                        id: "ebich"
+                    }
+                ],
+                selected: "ebich"
+            });
+            self.formValues.get("Category2").setUpdateFunc(self.getData);
+            self.formValues.get("Category3").setUpdateFunc(self.getAnotherData);
+            self.formValues.get("Category1").next = "Category2";
+            self.formValues.get("Category2").next = "Category3";
             self.formValues.set("description", {
                 id: "description",
                 label: "Описание",
@@ -219,7 +294,7 @@ export const DetailsSelect = observer(
                 variant="outlined"
                 disabled={field.disabled}
                 error={!!field.error}
-                style={{ marginTop: 16 }}
+                style={{ marginTop: 16, marginBottom: 16 }}
             >
                 {FieldComponent}
                 {field.error && <FormHelperText>{field.error}</FormHelperText>}
@@ -229,7 +304,7 @@ export const DetailsSelect = observer(
 );
 
 const AppView = observer((props) => {
-    console.log("Notice that parent rerenders only fiew times");
+    console.log("Notice that parent renders only once");
     return (
         <div>
             <div>
@@ -252,15 +327,17 @@ const AppView = observer((props) => {
                     })}
             </div>
             {props.store.hasFields && (
-                <button
+                <Button
                     onClick={() =>
                         props.store.formValues
-                            .get("statusId")
+                            .get("Category2")
                             .updateValues({ id: undefined })
                     }
+                    color="primary"
+                    variant="contained"
                 >
                     Update select values
-                </button>
+                </Button>
             )}
         </div>
     );
